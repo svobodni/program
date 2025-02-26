40e1db36c15765a7d36f01c8c797c92aa9f63373
Martin Ž <152999642+MaarthyGh@users.noreply.github.com>
2025-02-26 15:08:55 +0100

    Update getdate.rb

--- .pandoc/getdate.rb
+++ .pandoc/getdate.rb
@@ -2,4 +2,4 @@
 
 require 'yaml'
 
-puts YAML.load(File.read("README.md"), permitted_classes: [Date])['schvaleno'].strftime("%-d.%-m.%Y")
+puts YAML.safe_load(File.read("README.md"), permitted_classes: [Date])['schvaleno'].strftime("%-d.%-m.%Y")
